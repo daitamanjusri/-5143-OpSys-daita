d8f50854c88ec8c87bbfb2bb762027823d04b8c6
daitamanjusri <daitamanjusri@gmail.com>
2016-02-09 23:51:50 -0600

    Create random.sh

---
+++ random.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+WORDFILE="/usr/share/dict/words"
+NUMWORDS=10
+for i in 'sed $ NUMWORDS'
+do
+RDS=10
+for i in 'sed $ NUMWORDS'
+do
+rnum=$((RANDOM%NUMWORDS+1))
+sed -n "$rnum p" $WORDFILE
+done
+
